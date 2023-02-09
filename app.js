fc1e23ab6b0743aaa520675f0205ff516e2345ff
fernandog197 <bluewavesalta@gmail.com>
2023-02-09 14:07:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ const app = express()
 
 //middlewares
 app.set('trust proxy', 1)
-app.use(rateLimiter({
-  windowMs: 15*60*1000,
-  max: 100,
-}))
+// app.use(rateLimiter({
+//   windowMs: 15*60*1000,
+//   max: 100,
+// }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(express.json({ limit: '50mb' }))
 app.use(morgan('dev'))
